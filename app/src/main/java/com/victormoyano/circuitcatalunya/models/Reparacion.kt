0b029669904c3ac8fb310bfce895f9c9d86152a6
mohervictor <victorcrackball@gmail.com>
2024-05-03 17:12:03 +0200

    creartaskalayout

--- app/src/main/java/com/victormoyano/circuitcatalunya/models/Reparacion.kt
+++ app/src/main/java/com/victormoyano/circuitcatalunya/models/Reparacion.kt
@@ -1,7 +1,5 @@
 package com.victormoyano.circuitcatalunya.models
 
-import com.victormoyano.circuitcatalunya.R
-
 data class Reparacion(
     val topText1: String,
     val title: String,
@@ -9,9 +7,4 @@ data class Reparacion(
     val button1Text: String,
     val button2Text: String,
     val imageResource: Int
-)
-val myDataset = listOf(
-    Reparacion("SEGÜENT REPARACIÓN", "ZONA: CAMBI DE TECPRO", "MANTENIMIENTO PREVENTIVO", "POSPOSAR", "ASSIGNAR", R.drawable.info),
-    Reparacion("SEGÜENT REPARACIÓN", "ZONA: CAMBI DE TECPRO", "MANTENIMIENTO PREVENTIVO", "POSPOSAR", "ASSIGNAR", R.drawable.info),
-    // Agrega más objetos Reparacion aquí...
 )
