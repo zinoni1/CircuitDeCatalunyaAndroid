c5453f2e4283fb2f8dd87a52938e97e5110ce672
mohervictor <victorcrackball@gmail.com>
2024-05-09 19:44:10 +0200

    infoaveria

--- app/src/main/java/com/victormoyano/circuitcatalunya/LoginActivity.kt
+++ app/src/main/java/com/victormoyano/circuitcatalunya/LoginActivity.kt
@@ -1,13 +1,10 @@
 package com.victormoyano.circuitcatalunya
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
